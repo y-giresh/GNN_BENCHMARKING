53413465ce99f7af251655c27a4fa3d158ac4a34
y-giresh <yarrojugiresh@gmail.com>
2026-06-05 15:48:00 +0530

    Finished benchmark framework and fixed imports

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,45 +1,68 @@
-from torch_geometric.datasets import Planetoid
-from torch_geometric.datasets import TUDataset
+from torch_geometric.datasets import (
+    Planetoid,
+    TUDataset
+)
+
+from tasks.link_prediction import (
+    load_link_dataset
+)
 
 
-def get_dataset(dataset_name):
+def get_dataset(
+    dataset_name
+):
 
     dataset_name = dataset_name.lower()
+
 
     if dataset_name == "cora":
 
         return Planetoid(
+
             root="data",
+
             name="Cora"
+
         )
 
 
     elif dataset_name == "citeseer":
 
         return Planetoid(
+
             root="data",
+
             name="CiteSeer"
+
         )
 
 
     elif dataset_name == "pubmed":
 
         return Planetoid(
+
             root="data",
+
             name="PubMed"
+
         )
 
 
     elif dataset_name == "mutag":
 
         return TUDataset(
+
             root="data",
+
             name="MUTAG"
+
         )
 
 
     else:
 
         raise ValueError(
-            f"Dataset '{dataset_name}' not supported"
+
+            "Dataset not supported"
+
         )

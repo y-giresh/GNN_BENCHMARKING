612c88302750477e086d293d41c9b5c37c7406c4
y-giresh <yarrojugiresh@gmail.com>
2026-06-17 15:48:27 +0530

    hyperparameters ok!!

--- run_hyperparameter.bat
+++ run_hyperparameter.bat
@@ -1,14 +1,14 @@
 @echo off
 set HYPER=1
 
-echo ===================
+echo =================== 
 echo NODE CLASSIFICATION
 echo ===================
 
 python main.py node gcn cora 32 0.01 0.5 0.0005 200
-python main.py node gcn cora 64 0.01 0.5 0.0005 200
-python main.py node gcn cora 32 0.005 0.5 0.0005 200
-python main.py node gcn cora 32 0.01 0.3 0.0005 200
+python main.py node gat cora 64 0.01 0.5 0.0005 200
+python main.py node graphsage cora 32 0.005 0.5 0.0005 200
+python main.py node gin cora 128 0.01 0.3 0.0005 200
 
 
 echo ===================
@@ -16,19 +16,19 @@ echo LINK PREDICTION
 echo ===================
 
 python main.py link gcn cora 32 0.01 0.5 0.0005 200
-python main.py link gcn cora 64 0.01 0.5 0.0005 200
-python main.py link gcn cora 32 0.005 0.5 0.0005 200
-python main.py link gcn cora 32 0.01 0.3 0.0005 200
+python main.py link gin cora 64 0.01 0.5 0.0005 200
+python main.py link gat cora 128 0.005 0.5 0.0005 200
+python main.py link graphsage cora 32 0.01 0.3 0.0005 200
 
 
 echo ===================
 echo GRAPH CLASSIFICATION
 echo ===================
 
-python main.py graph gcn mutag 32 0.01 0.5 0.0005 200
-python main.py graph gcn mutag 64 0.01 0.5 0.0005 200
-python main.py graph gcn mutag 32 0.005 0.5 0.0005 200
-python main.py graph gcn mutag 32 0.01 0.3 0.0005 200
+python main.py graph gcn mutag 128 0.01 0.5 0.0005 200
+python main.py graph gat mutag 64 0.01 0.5 0.0005 200
+python main.py graph gin mutag 32 0.005 0.5 0.0005 200
+python main.py graph graphsage mutag 32 0.01 0.3 0.0005 200
 
 
 echo ===================
@@ -36,8 +36,8 @@ echo EXTRA CHECKS
 echo ===================
 
 python main.py node gcn cora 32 0.01 0.5 0.0001 200
-python main.py link gcn cora 32 0.01 0.5 0.0001 200
-python main.py graph gcn mutag 32 0.01 0.5 0.0001 200
+python main.py link graphsage cora 32 0.01 0.5 0.0001 200
+python main.py graph gin mutag 32 0.01 0.5 0.0001 200
 
 
 echo COMPLETE
